2ca847dd307cef923c8841f918895a7e94674ac0
makbarzidane <akbarzidane12@gmail.com>
2024-12-05 00:20:55 +0700

    first commit

---
+++ app/Views/registrasi.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informasi Akun</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="form-container">
+        <h1>Informasi Akun</h1>
+        <form>
+            <div class="form-group">
+                <input type="text" placeholder="*Nama Lengkap">
+            </div>
+            <div class="form-group">
+                <input type="email" placeholder="*Email">
+            </div>
+            <div class="form-group">
+                <input type="tel" placeholder="*No. Telepon">
+            </div>
+            <div class="form-group">
+                <input type="password" placeholder="*Password">
+            </div>
+            <div class="form-group">
+                <input type="password" placeholder="*Konfirmasi Password">
+            </div>
+            <a href="#">Ubah informasi?</a>
+            <button type="button">Log Out</button>
+        </form>
+    </div>
+</body>
+</html>
