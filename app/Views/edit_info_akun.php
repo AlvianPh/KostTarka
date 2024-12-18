13f8408a3fd4ec41e7d00a611f98ed60230f1965
YoriCode11 <heatcliff12345@gmail.com>
2024-12-18 19:49:50 +0700

    membuat halaman laporan keuangan

--- app/Views/edit_info_akun.php
+++ app/Views/edit_info_akun.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Edit informasi Akun</title>
+    <title>Form Registrasi</title>
     <link rel="stylesheet" href="css/edit_info_akun.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   </head>
@@ -18,7 +18,7 @@
               <li><a href="<?= base_url('penghuni-kos') ?>">List Kamar</a></li>
               <li><a href="#">Pembayaran</a></li>
               <li><a href="#">
-                      <i class="fas fa-user-circle" style="font-size: 24px; color: white;"></i>
+                      <i class="fas fa-user-circle" style="font-size: 24px; color: #007bff;"></i>
               </a></li>
           </ul>
       </nav>
---
+++ app/Views/laporan_keuangan.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="id">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Laporan Keuangan - TARKA</title>
+    <link rel="stylesheet" href="css/laporan_keuangan.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+    />
+  </head>
+  <body>
+    <header class="header">
+      <div class="logo">
+        <img src="image/logo 1.png" alt="Kost Tarka Logo" />
+      </div>
+      <nav>
+        <ul>
+          <li><a href="<?= base_url('/') ?>">Home</a></li>
+          <li><a href="<?= base_url('penghuni-kos') ?>">List Kamar</a></li>
+          <li><a href="#">Pembayaran</a></li>
+          <li>
+            <a href="#">
+              <i
+                class="fas fa-user-circle"
+                style="font-size: 24px; color: #007bff"
+              ></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <div class="riwayat">
+      <h2>Riwayat Pembayaran</h2>
+      <form>
+        <table>
+          <tr>
+            <td><label for="tanggal-awal">Tanggal Awal</label></td>
+            <td>
+              <input
+                type="date"
+                id="tanggal-awal"
+                name="tanggal-awal"
+                required
+              />
+            </td>
+          </tr>
+          <tr>
+            <td><label for="tanggal-akhir">Tanggal Akhir</label></td>
+            <td>
+              <input
+                type="date"
+                id="tanggal-akhir"
+                name="tanggal-akhir"
+                required
+              />
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="button-container">
+                <button type="submit">Tampilkan</button>
+              </div>
+            </td>
+          </tr>
+        </table>
+      </form>
+    </div>
+
+    <div class="container">
+      <div class="card">
+        <span>Kamar 1A<br />21/01/2024</span>
+        <span>Nama Penghuni</span>
+        <span>Lunas <br /><img src="icon.png" alt="Image" /></span>
+      </div>
+      <div class="card">
+        <span>Kamar 1A<br />21/01/2024</span>
+        <span>Nama Penghuni</span>
+        <span>Lunas <br /><img src="icon.png" alt="Image" /></span>
+      </div>
+      <div class="card">
+        <span>Kamar 1A<br />21/01/2024</span>
+        <span>Nama Penghuni</span>
+        <span>Lunas <br /><img src="icon.png" alt="Image" /></span>
+      </div>
+      <div class="card">
+        <span>Kamar 1A<br />21/01/2024</span>
+        <span>Nama Penghuni</span>
+        <span>Lunas <br /><img src="icon.png" alt="Image" /></span>
+      </div>
+    </div>
+  </body>
+</html>
