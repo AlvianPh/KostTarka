3646e542151bedc654a6d7e3515c68eb7c8ec73a
YoriCode11 <heatcliff12345@gmail.com>
2024-12-18 19:58:39 +0700

    perbaikan

--- app/Views/edit_info_akun.php
+++ app/Views/edit_info_akun.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Form Registrasi</title>
+    <title>Edit informasi Akun</title>
     <link rel="stylesheet" href="css/edit_info_akun.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   </head>
